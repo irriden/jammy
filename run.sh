c9ada66b49729887ceceffe8cd144624c09900f4
irriden <irriden@proton.me>
2024-04-21 16:44:10 +0000

    script with no interaction

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-apt install protobuf-compiler rustup git
+apt install -y protobuf-compiler rustup git
 ./connect_nodes.sh 2> /dev/null
 ./fund.sh
-git clone https://github.com/irriden/jammy.git
+git clone https://github.com/irriden/jammy.git --branch morning
 cd jammy
 git clone --branch attackathon https://github.com/carlaKC/lnd.git
 export LND_REPO_DIR=/jammy/lnd
