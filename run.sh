e287afd97bde450e0b4976fd85b4bd8da33ecaf1
irriden <irriden@proton.me>
2024-04-29 15:33:25 +0000

    update pulled branch in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 apt install -y protobuf-compiler rustup git
 ./connect_nodes.sh 2> /dev/null
 ./fund.sh
-git clone https://github.com/irriden/jammy.git --branch morning
+git clone https://github.com/irriden/jammy.git --branch endorsed-fast-jamming
 cd jammy
 git clone --branch attackathon https://github.com/carlaKC/lnd.git
 export LND_REPO_DIR=/jammy/lnd
